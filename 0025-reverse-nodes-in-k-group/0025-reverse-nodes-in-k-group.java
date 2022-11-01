ef1a5bdf6f35875b2b30f75f5eb0954f7edca8c7
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2022-11-01 14:42:48 +0900

    Time: 4 ms (11.45%), Space: 46.2 MB (11.43%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head) {
+        int n = 0;
+        ListNode cur = head;
+        if (head == null) {
+            return null;
+        }
+        while (cur != null) {
+            n++;
+            cur = cur.next;
+        }
+        if (n == 1) {
+            return head;
+        }
+        cur = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head.next = null;
+        return prev;
+
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        int num = 1;
+        ListNode next = null;
+        ListNode answer = null;
+        ListNode prevReverseNode = null;
+        while (cur != null) {
+            if (num == k) {
+                next = cur.next;
+                cur.next = null;
+                ListNode reverseNode = reverse(head);
+                if (prevReverseNode != null) {
+                    for (int i = 0; i < k - 1; i++) {
+                        prevReverseNode = prevReverseNode.next;
+                    }
+                    prevReverseNode.next = reverseNode;
+                } else {
+                    answer = reverseNode;
+                }
+                prevReverseNode = reverseNode;
+                num = 1;
+                cur = next;
+                head = cur;
+            } else {
+                num++;
+                cur = cur.next;
+            }
+        }
+        if (num <= k) {
+            if (prevReverseNode != null) {
+                for (int i = 0; i < k - 1; i++) {
+                    prevReverseNode = prevReverseNode.next;
+                }
+                prevReverseNode.next = head;
+            }
+        }
+        return answer;
+    }
+}
