0126e7b29d9a87dd585afaabd2515e5b470b94c5
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2022-11-02 17:51:43 +0900

    Time: 6 ms (66.76%), Space: 44.1 MB (34.14%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> answer = new ArrayList<>();
+        backtracking(0, target, candidates, new LinkedList<Integer>(), answer);
+        return answer;
+    }
+    
+    public void backtracking(int start, int target, int[] candidates, LinkedList<Integer> cur, List<List<Integer>> answer) {
+        if(target == 0) {
+            answer.add(new ArrayList<Integer>(cur));
+        } else {
+            for(int i = start; i < candidates.length; i++) {
+                if(i > start && candidates[i] == candidates[i - 1]) continue;
+                if(target - candidates[i] < 0) return;
+                cur.addLast(candidates[i]);
+                backtracking(i + 1, target - candidates[i], candidates, cur, answer);
+                cur.removeLast();
+            }
+        }
+    }
+}
